d6f43e295b85e8bc381a5da04b729c440fcc491a
kieff-boop <kiefferkylebulahao@gmail.com>
2026-05-02 18:52:14 +0800

    fix supabase connection

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://ckpqjledxzroqweebmaa.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrcHFqbGVkeHpyb3F3ZWVibWFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3MTAxOTksImV4cCI6MjA5MzI4NjE5OX0.W-W6r928B8PCPdiEnxyPYuB9S_Dr1s79lLlA1-Vad3g";
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
